38922175488ce400c49354db1020b34ce2bc0ab4
CheamNorakpanha <cheam.panha168@gmail.com>
2023-10-05 23:03:33 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cheamnorakpanha.github.io
---
+++ config.js
@@ -0,0 +1,73 @@
+window.PorfolioConfig = {
+  name: 'Cheam Norakpanha',
+  jobTitle: 'Full-stack Web Developer',
+  jobDescription: '(in progress...)',
+  skills: [
+    {
+      name: 'JavaScript',
+      description: 'Experienced in leveraging JavaScript frameworks and libraries to enhance user interfaces and overall functionality.'
+    },
+    {
+      name: 'HTML',
+      description: 'Skillful in creating scalable and maintainable markup, adhering to industry best practices and standards.'
+    },
+    {
+      name: 'CSS',
+      description: 'Proficient in CSS for styling and layout, ensuring visually appealing and responsive web designs.'
+    }
+  ],
+  works: [
+    {
+      name: 'A Game Developer',
+      link: '',
+      description: 'I work as a part-time game developer at Roblox Co. since I enjoy playing this game in my spare time.'
+    },
+    {
+      name: 'error404',
+      link: '',
+      description: 'Error404 not found'
+    },
+    {
+      name: 'error404',
+      link: '',
+      description: 'Error404 not found'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Playing the Guitar',
+      description: 'Playing the guitar is a fantastic way to relieve stress and entertain yourself.'
+    },
+    {
+      name: 'PLaying Games',
+      description: 'Video games can be both entertaining and mentally stimulating, providing challenges and problem-solving opportunities. Multiplayer games also offer a social aspect, allowing me to connect with friends or make new ones online.'
+    },
+    {
+      name: 'Listening to Music',
+      description: 'Listening to music can be a great way to unwind and relax. It has the ability to reduce stress and elevate my mood.'
+    },
+    {
+      name: 'Doing Research at the Library',
+      description: 'Researching allows me to delve into topics of personal interest, fostering a continuous process of learning and discovery.'
+    }
+  ],
+  contacts: {
+    email: 'cheam.panha168@gmail.com',
+    // mobileNo: '+639171234567',
+    phoneNo: '+855 92 289 441',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/CheamNorakpanha'
+      },
+      {
+        name: 'Spotify',
+        link: 'https://open.spotify.com/user/thelegendofblue?si=30b41bf1247a459a'
+      },
+      {
+        name: 'Twitter',
+        link: 'https://www.tiktok.com/@deathkombat41567/video/7252944853686848811'
+      }
+    ]
+  }
+};
